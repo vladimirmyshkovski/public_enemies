8d82b9b2ae74a0bdbe8849856ef74575dbb97760
narnikgamarnikus <narnikgamarnikus@gmail.com>
2017-06-20 03:28:52 +0300

    changed env.example

--- config/settings/production.py
+++ config/settings/production.py
@@ -73,7 +73,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['xn------dddfnxoenlfghchl4bitc.xn--90ais'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['xn------dddfnxoenlfghchl4bitc.xn--90ais', '37.143.8.19'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
